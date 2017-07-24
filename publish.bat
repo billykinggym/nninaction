2a2838ce775a263311136b5ede1118c112b6a401
billy <billykinggym@126.com>
2017-07-24 23:38:20 +0800

    add sync script

---
+++ publish.bat
@@ -0,0 +1,9 @@
+F:\tools\BeyondCompare4\BCompare.exe @sync.txt
+
+cd E:\SOHO_Working\自媒体\git-pages\自己动手写神经网络\gh-pages
+
+git add -A
+git commit -m "create by publish.bat"
+git push origin
+git push oschina
+
